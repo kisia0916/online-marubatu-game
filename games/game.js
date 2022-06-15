1593749fd869c9a2e6a6b9abd41d642e6599a36b
fumifumi <satouabe30@gmail.com>
2022-06-15 18:36:51 +0900

    test

--- games/game.js
+++ games/game.js
@@ -69,12 +69,6 @@ socket_1.on("distract_sesstion",(user)=>{
         window.sessionStorage.setItem(['key4'],"");
         window.sessionStorage.setItem(['key5'],"");
     }
-})
-history.replaceState(null, document.getElementsByTagName('title')[0].innerHTML, null);
-window.addEventListener('popstate', function(e) {
-  socket_1.on("disconnect",()=>{
-    
-  })
 });
 /////////////////////////////////////////////////////
 function get_mouse(e){

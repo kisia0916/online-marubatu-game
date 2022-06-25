939297bc030e8d5abbd2c1d196921fab0ad906c0
fumifumi <satouabe30@gmail.com>
2022-06-25 19:04:16 +0900

    y\mazi

--- games/game.js
+++ games/game.js
@@ -67,7 +67,7 @@ socket_1.on("end_koma_server",(mess)=>{
         const d1 = new Date();
         while (true) {
         const d2 = new Date();
-        if (d2 - d1 > 300) {
+        if (d2 - d1 > 200) {
             play_count = 1;
         if (ban == 1){
             //turn_counter = 0;

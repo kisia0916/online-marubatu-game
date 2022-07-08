33b118d56d13078fdba743c9a060a8db9fe00a29
fumifumi <satouabe30@gmail.com>
2022-07-08 22:57:00 +0900

    kkokoko

--- games/game.js
+++ games/game.js
@@ -730,10 +730,11 @@ function main_program_3(){
                 console.log("vvvvvvvvvvvvvvvv"+koma_counter_2);
                 if (koma_counter_2>koma_counter_1){
                     if (turn_change_counter == 0){
+                        turn_change_counter = 1;
                         socket_1.emit("end_koma",window.sessionStorage.getItem(['k2']));
                         window.sessionStorage.setItem(['k90'],"");
                         window.sessionStorage.setItem(['k100'],"");
-                        turn_change_counter = 1;
+
                         koma_oki = 0;
                         play_count = 0;
                 }
